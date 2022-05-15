ab61db0784035f468a2b6e21b629dee7e7f28ece
Sergio Lopez <sergio@softnya.com>
2022-05-15 18:27:51 -0500

    Error al enviar fetch

--- assets/js/fetchFormProject.js
+++ assets/js/fetchFormProject.js
@@ -21,3 +21,36 @@ fetch("https://api.fondeaeloceano.xyz/v1/categories")
         });
         categories.innerHTML = catOption;
 });
+
+
+document.forms[0].onsubmit = async function( event ) {
+    event.preventDefault();
+    //const formData = new URLSearchParams(new FormData(this));
+    let formData = [];
+    const fields = document.querySelectorAll(".uk-form *[name]");
+    fields.forEach(field => {
+        formData.push({
+            [field.name]: field.value
+        });
+    });
+    console.log(JSON.stringify(formData));
+    fetch("https://api.fondeaeloceano.xyz/v1/projects",
+        {   
+            method: 'POST',
+            mode : 'no-cors',
+            credentials: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: formData
+        })
+        .then(response => {
+            console.log(response);
+            if(response.status == 200)
+                response.json();
+        })
+        .then(data => {
+            console.log(data);
+        });
+  };
